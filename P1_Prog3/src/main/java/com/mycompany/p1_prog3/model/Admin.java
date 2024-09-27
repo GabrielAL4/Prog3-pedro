90b6da05f3bb98e321b559d47474a8fe45bb206c
Gabriel Albuquerque <81835118+GabrielAL4@users.noreply.github.com>
2024-09-27 09:16:05 -0300

    Update Admin.java

--- P1_Prog3/src/main/java/com/mycompany/p1_prog3/model/Admin.java
+++ P1_Prog3/src/main/java/com/mycompany/p1_prog3/model/Admin.java
@@ -1,20 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.p1_prog3.model;
 
 import java.util.Date;
 
-/**
- *
- * @author peanj
- */
-public class Admin extends Usuario
-{
-    
+public class Admin extends Usuario {
+
+    // O construtor de Admin deve sempre definir isAdmin como true
     public Admin(String nomeCompleto, String username, String senha, Date dataCadastro) {
-        super(nomeCompleto, username, senha, dataCadastro);
+        super(nomeCompleto, username, senha, dataCadastro, true);  // Passa 'true' para isAdmin
     }
     
 }
