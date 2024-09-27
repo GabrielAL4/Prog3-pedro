4a38d68e33cb54bd202706a4d87f0b93d50a9143
Gabriel Albuquerque <81835118+GabrielAL4@users.noreply.github.com>
2024-09-27 08:17:13 -0300

    Update DaoAdmin.java

--- P1_Prog3/src/main/java/com/mycompany/p1_prog3/dao/DaoAdmin.java
+++ P1_Prog3/src/main/java/com/mycompany/p1_prog3/dao/DaoAdmin.java
@@ -1,3 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.mycompany.p1_prog3.dao;
+
+/**
+ *
+ * @author peanj
+ */
 import com.mycompany.p1_prog3.model.Admin;
 import com.fasterxml.jackson.core.type.TypeReference;
 
