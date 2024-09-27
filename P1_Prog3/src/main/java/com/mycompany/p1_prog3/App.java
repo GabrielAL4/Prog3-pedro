fe69527e9068721374713ca569b269a543bbe083
Gabriel Albuquerque <81835118+GabrielAL4@users.noreply.github.com>
2024-09-27 09:36:31 -0300

    Update App.java

--- P1_Prog3/src/main/java/com/mycompany/p1_prog3/App.java
+++ P1_Prog3/src/main/java/com/mycompany/p1_prog3/App.java
@@ -1,24 +1,15 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.p1_prog3;
 
-/**
- *
- * @author peanj
- */
+import com.mycompany.p1_prog3.GUI.Tela_Login;
 
-import javax.swing.*;
-
-public class App {
+public class Main {
 
     public static void main(String[] args) {
-        GuiMenu guiMenu = new GuiMenu();
-        JFrame frame = new JFrame("Menu");
-        frame.setContentPane(guiMenu.JPMenu);
-        frame.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
-        frame.pack();
-        frame.setVisible(true);
+        // Inicializar a interface de login
+        Tela_Login telaLogin = new Tela_Login();
+        telaLogin.setVisible(true);  // Exibir a tela de login
+        
+        // O que fazer após o login bem-sucedido pode ser colocado aqui.
+        // Isso pode incluir carregar a tela principal do sistema ou continuar a execução.
     }
 }
